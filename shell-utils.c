a1e408d82f2b1aa1133e971ea8b4ccebf3909fdb
raphaeldott <raphael.dott@hotmail.com>
2025-01-06 13:35:22 +0100

    betty

--- shell-utils.c
+++ shell-utils.c
@@ -14,23 +14,23 @@ char *find_executable(char *command)
 	if (command[0] == '/' || command[0] == '.')
 	{
 		if (access(command, X_OK) == 0)
-			return strdup(command);
-		return NULL;
+			return (strdup(command));
+		return (NULL);
 	}
 
 	path = getenv("PATH");
 	if (!path)
-		return NULL;
+		return (NULL);
 
 	path_copy = strdup(path);
 	if (!path_copy)
-		return NULL;
+		return (NULL);
 
 	full_path = malloc(1024);
 	if (!full_path)
 	{
 		free(path_copy);
-		return NULL;
+		return (NULL);
 	}
 
 	dir = strtok(path_copy, ":");
@@ -40,19 +40,19 @@ char *find_executable(char *command)
 		if (access(full_path, X_OK) == 0)
 		{
 			free(path_copy);
-			return full_path;
+			return (full_path);
 		}
 		dir = strtok(NULL, ":");
 	}
 
 	free(path_copy);
 	free(full_path);
-	return NULL;
+	return (NULL);
 }
 /**
  * execute_command - Executes the command entered by the user.
- * @command: The command to execute.
  * @environment_var: The environment variables.
+ * @argv: the prompt
  *
  * This function fork a child process to execute the
  * command entered by the user.
@@ -60,47 +60,6 @@ char *find_executable(char *command)
  * The parent process waits for the child to finish.
  * If execve fails, an error message is printed.
  */
-/*void execute_command(char **argv, char **environment_var)
-  {
-  pid_t pid;
-  char *executable = find_executable(argv[0]);
-
-  if (executable == NULL)
-  {
-  fprintf(stderr, "%s: No such file or directory\n", argv[0]);
-  return;
-  }
-
-  argv[0] = executable;
-  argv[1] = NULL;
-
-  pid = fork();
-
-  if (pid == -1)
-  {
-  perror("fork");
-  free(argv[0]);
-  free(executable);
-  exit(EXIT_FAILURE);
-  }
-  else if (pid == 0)
-  {
-  if (execve(argv[0], argv, environment_var) == -1)
-  {
-  perror(argv[0]);
-  free(executable);
-  exit(EXIT_FAILURE);
-  }
-  }
-  else
-  {
-  wait(NULL);
-  }
-
-  free(executable);
-  }
-  */
-
 void execute_command(char **argv, char **environment_var)
 {
 	pid_t pid;
